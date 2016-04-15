cb19c58051e85fd934b8b2e7bca7b46fb8445677
Juhani <jrunila@gmail.com>
2016-04-15 11:20:48 +0300

    Update siisen.js
    Changed to jquery

--- siisen.js
+++ siisen.js
@@ -1,8 +1,8 @@
-$(function() {
+jquery(function() {
   // run the currently selected effect
     function runEffect() {
       // get effect type from
-      var selectedEffect = $( "#effectTypes" ).val();
+      var selectedEffect = jquery( "#effectTypes" ).val();
  
       // most effect types need no options passed by default
       var options = {};
@@ -13,33 +13,33 @@ $(function() {
         options = { to: { width: 480, height: 385 } };
       }
        // run the effect
-      $( "#effect" ).show( selectedEffect, options, 1100, callback );
+      jquery( "#effect" ).show( selectedEffect, options, 1100, callback );
     };
  
     //callback function to bring a hidden box back
     function callback() {
       setTimeout(function() {
-        $( "#effect:visible" ).removeAttr( "style" ).fadeOut();
+        jquery( "#effect:visible" ).removeAttr( "style" ).fadeOut();
         }, 10000 );
     };
  
     // set effect from select menu value
-    $( "#button" ).click(function() {
+    jquery( "#button" ).click(function() {
       runEffect();
     });
  
-    $( "#effect" ).hide();
+    jquery( "#effect" ).hide();
   
     //hide v_nav_bar
      //$( "#v_nav_bar").show();
     
-    $(function() {
-      $( "#accordion" ).accordion();
+    jquery(function() {
+      jquery( "#accordion" ).accordion();
       })
     });
     
-    $( "#clickme" ).click(function() {
-      $( "#back" ).slideToggle( "slow", function() {
+    jquery( "#clickme" ).click(function() {
+      jquery( "#back" ).slideToggle( "slow", function() {
     // Animation complete.
   });
 });
