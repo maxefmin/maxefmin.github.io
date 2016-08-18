345db5d58e30ca8c4da70069ef58d6d6d64a0323
Juhani <Runila>
2016-08-18 22:14:03 +0300

    Update siisen.js
    All the mess

--- siisen.js
+++ siisen.js
@@ -1,19 +1,12 @@
-
-$(function() {
-  // run the currently selected effect
-    function runEffect() {
-      // get effect type from
+$(function(){
+    function runEffect(){      
       var selectedEffect = $( "#effectTypes" ).val();
- 
-      // most effect types need no options passed by default
       var options = {};
-      // some effects have required parameters
       if ( selectedEffect === "scale" ) {
         options = { percent: 60 };
       } else if ( selectedEffect === "size" ) {
         options = { to: { width: 480, height: 385 } };
       }
-       // run the effect
       $( "#effect" ).show( selectedEffect, options, 1100, callback );
     };
  
@@ -23,16 +16,12 @@ $(function() {
         $( "#effect:visible" ).removeAttr( "style" ).fadeOut();
         }, 10000 );
     };
- 
-    // set effect from select menu value
     $( "#button" ).click(function() {
       runEffect();
     });
  
     $( "#effect" ).hide();
-  
-    //hide v_nav_bar
-     //$( "#v_nav_bar").show();
+    $( "#v_nav_bar").show();
     
     $(function() {
       $( "#accordion" ).accordion({
